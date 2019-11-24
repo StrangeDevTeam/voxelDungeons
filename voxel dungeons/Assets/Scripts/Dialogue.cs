5d0878e1b845f8d2bae6c68838d31a76398affab
arcturus125 <arcturus1259@gmail.com>
2019-11-24 00:19:14 +0000

    started to build erivaleCastle and implemented DialogueChoice
    users can now press keys 1-5 to choose between a number of dialogue options

--- voxel dungeons/Assets/Scripts/Dialogue.cs
+++ voxel dungeons/Assets/Scripts/Dialogue.cs
@@ -5,32 +5,96 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    public static bool isInDialogue = false;
-    public static KeyCode nextKey = KeyCode.G;
-    public static Dialogue currentDialogue;
+    /// Static
+    public static bool isInDialogue = false; // is the user currently in a dialogue with an entitiy?
+    public static KeyCode nextKey = KeyCode.F; // the key the user presses to move on to the next dialogue
+    public static Dialogue currentDialogue; // a link to the current or last dialogue the user has active
 
+    /// public 
     [HideInInspector]
-    public string text;
+    public string text; // the contents of the dialogue
     [HideInInspector]
-    public Dialogue nextDialogue = null;
-
+    public Dialogue nextDialogue = null; // the link to the next dialogue if there is one
+    [HideInInspector]
+    public bool pauseForFrame = true; // stops the program from ruching ahead, when true the program will wait 1 frame before monitoring inputs
 
     void LateUpdate()
     {
-        if (isInDialogue)
+
+        try
         {
-            if (Input.GetKeyDown(KeyCode.F)) 
+            DialogueChoice currentDialogueChoice = (DialogueChoice)currentDialogue;
+            Debug.Log("fuck");
+            if (!currentDialogue.pauseForFrame)
             {
-                try
+                if (isInDialogue)
                 {
-                    currentDialogue.nextDialogue.ShowDialogue();
-                }
-                catch (Exception exc)
-                {
-                    currentDialogue.HideDialogue();
+                    try
+                    {
+                        if (Input.GetKeyDown(DialogueChoice.option1))
+                        {
+                            currentDialogueChoice.nextBranches[0].ShowDialogue();
+                        }
+                        else if (Input.GetKeyDown(DialogueChoice.option2))
+                        {
+                            currentDialogueChoice.nextBranches[1].ShowDialogue();
+                        }
+                        else if (Input.GetKeyDown(DialogueChoice.option3))
+                        {
+                            currentDialogueChoice.nextBranches[2].ShowDialogue();
+                        }
+                        else if (Input.GetKeyDown(DialogueChoice.option4))
+                        {
+                            currentDialogueChoice.nextBranches[3].ShowDialogue();
+                        }
+                        else if (Input.GetKeyDown(DialogueChoice.option5))
+                        {
+                            currentDialogueChoice.nextBranches[4].ShowDialogue();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        currentDialogue.HideDialogue();
+                    }
                 }
             }
+            else
+            {
+                currentDialogue.pauseForFrame = false;
+            }
         }
+
+        catch (Exception)
+        {
+            try
+            {
+                if (!currentDialogue.pauseForFrame)
+                {
+                    if (isInDialogue)
+                    {
+                        if (Input.GetKeyDown(nextKey))
+                        {
+                            try
+                            {
+                                currentDialogue.nextDialogue.ShowDialogue();
+                            }
+                            catch (Exception)
+                            {
+                                currentDialogue.HideDialogue();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    currentDialogue.pauseForFrame = false;
+                }
+            }
+            catch (Exception)
+            { }
+        }
+
+
     }
 
     public Dialogue(string dialogueText)
@@ -42,15 +106,44 @@ public class Dialogue : MonoBehaviour
         text = dialogueText;
         nextDialogue = next;
     }
+
+
     public void ShowDialogue()
     {
         currentDialogue = this;
         UIController.ShowDialogueBox(text);
         isInDialogue = true;
+        pauseForFrame = true;
     }
     public void HideDialogue()
     {
         UIController.HideDialogueBox();
         isInDialogue = false;
+    }
+}
+public class DialogueChoice : Dialogue
+{
+
+    public static KeyCode option1 = KeyCode.Alpha1;
+    public static KeyCode option2 = KeyCode.Alpha2;
+    public static KeyCode option3 = KeyCode.Alpha3;
+    public static KeyCode option4 = KeyCode.Alpha4;
+    public static KeyCode option5 = KeyCode.Alpha5;
+
+
+    [HideInInspector]
+    public string[] choices;
+    [HideInInspector]
+    public Dialogue[] nextBranches;
+
+    public DialogueChoice( string dialogueText, string[] dialogueChoices , Dialogue[] nextDialogueBranches) : base(dialogueText)
+    {
+        choices = dialogueChoices;
+        nextBranches = nextDialogueBranches;
+    }
+
+    void LateUpdate()
+    {
+        
     }
 }
--- voxel dungeons/Assets/Scripts/ScriptedObjects/dialogueDemo.cs
+++ voxel dungeons/Assets/Scripts/ScriptedObjects/dialogueDemo.cs
@@ -8,11 +8,18 @@ public class dialogueDemo : MonoBehaviour
     Dialogue test2;
     Dialogue testDialogue;
 
+
+    DialogueChoice testChoice;
+
     void Start()
     {
         test3 = new Dialogue("testes");
         test2 = new Dialogue("And this is another test", test3);
         testDialogue = new Dialogue("Hello there, This is a test", test2);
+
+        string[] choices = { "option 1", "option 2"};
+        Dialogue[] dChoices = { testDialogue, test3 };
+        testChoice = new DialogueChoice("make your choice", choices, dChoices);
 
     }
 
@@ -28,5 +35,6 @@ public class dialogueDemo : MonoBehaviour
     public void NoLongerNearby()
     {
         testDialogue.HideDialogue();
+        UIController.HideInteractionTooltip();
     }
 }
